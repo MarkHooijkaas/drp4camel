2f81577dde6416a4fd69caf5a144cfb71d72d74c
Mark Hooijkaas <mark@hooijkaas.net>
2016-08-24 09:57:53 +0200

    removed some debugging println's

--- src/main/java/org/kisst/drp4camel/RouteLoader.java
+++ src/main/java/org/kisst/drp4camel/RouteLoader.java
@@ -86,13 +86,13 @@ public class RouteLoader {
 		try (InputStream is = new FileInputStream(f)) {
 			RoutesDefinition routes = context.loadRoutesDefinition(is);
 			//System.out.println(r.getDescription().getLang());
-			System.out.println("SHORT:"+routes.getShortName());
-			System.out.println("LABEL:"+routes.getLabel());
+			//System.out.println("SHORT:"+routes.getShortName());
+			//System.out.println("LABEL:"+routes.getLabel());
 
 			for (RouteDefinition r:routes.getRoutes()) {
 				//System.out.println(r.getShortName());
-				System.out.println(r.getGroup());
-				System.out.println(r.getLabel());
+				//System.out.println(r.getGroup());
+				//System.out.println(r.getLabel());
 				//for (String key:r.getProperties().keySet())
 				//	System.out.println(key+"\t"+r.getProperties().get(key));
 
